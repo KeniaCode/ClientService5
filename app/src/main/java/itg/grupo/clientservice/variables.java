3d1925d1b2b054e87b57550e2cd22c58718a5e32
KevinAdiel <201020724@ingenieria.usac.edu.gt>
2017-02-12 12:47:07 -0600

    Archivos faltas y librerias que sirven para el consumo del servicio web y otras funcionalidades.

---
+++ app/src/main/java/itg/grupo/clientservice/variables.java
@@ -0,0 +1,17 @@
+package itg.grupo.clientservice;
+
+/**
+ * Created by Kevin on 22/01/2017.
+ */
+
+public class variables {
+
+    public static String token;
+    public static String sistema = "2";
+    public static String venta;
+
+    public static String URL = "http://67.205.153.106/lib/webservice.php?wsdl";
+    public static String NAMESPACE = "http://67.205.153.106";
+    public static String SOAP_ACTION = "http://67.205.153.106/lib";
+    public static String usuario="";
+}
