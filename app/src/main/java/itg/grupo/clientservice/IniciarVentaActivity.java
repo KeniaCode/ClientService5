20b604ed79be0c17929dfbdb552f5ec47456a838
kenia <kenia.apk@gmail.com>
2017-02-18 12:29:36 -0600

    Opciones de no venta completadas.

--- app/src/main/java/itg/grupo/clientservice/IniciarVentaActivity.java
+++ app/src/main/java/itg/grupo/clientservice/IniciarVentaActivity.java
@@ -10,6 +10,7 @@ import android.os.Environment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,9 +25,6 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONTokener;
 import org.ksoap2.SoapEnvelope;
 import org.ksoap2.serialization.PropertyInfo;
 import org.ksoap2.serialization.SoapObject;
@@ -35,9 +33,9 @@ import org.ksoap2.transport.HttpTransportSE;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 import itg.grupo.clientservice.model.AudioChannel;
 import itg.grupo.clientservice.model.AudioSampleRate;
 import itg.grupo.clientservice.model.AudioSource;
@@ -48,7 +46,6 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
-import okio.ByteString;
 
 
 public class IniciarVentaActivity extends AppCompatActivity {
@@ -56,7 +53,12 @@ public class IniciarVentaActivity extends AppCompatActivity {
     private static final int REQUEST_RECORD_AUDIO = 0;
     private static String AUDIO_FILE_PATH =
             Environment.getExternalStorageDirectory().getPath() + "/audio_venta.wav";
-   // Spinner _spinner_talla_ropa;
+    // Spinner _spinner_talla_ropa;
+    Spinner spinner_talla_ropa;
+    Spinner spinner_colores;
+    EditText otras_especificaciones;
+    CheckBox checkBoxTalla;
+    CheckBox checkBoxColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,6 +75,19 @@ public class IniciarVentaActivity extends AppCompatActivity {
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // TODO Auto-generated method stub
+        if( (keyCode==KeyEvent.KEYCODE_HOME))
+        {
+            // *** DO YOUR STUFF HERE ***
+            return true;
+        }else{
+        return super.onKeyDown(keyCode, event);
+        }
     }
 
     @Override
@@ -137,7 +152,7 @@ public class IniciarVentaActivity extends AppCompatActivity {
 
                 Toast.makeText(this, "Audio recorded successfully!", Toast.LENGTH_SHORT).show();
 
-                uploadFile( requestCode, resultCode, data);
+                uploadFile(requestCode, resultCode, data);
 
                 seleccionarResultado();
 
@@ -185,23 +200,41 @@ public class IniciarVentaActivity extends AppCompatActivity {
         alertDialogBuilder.setCancelable(false);
 
 
-        final Spinner spinner_tipo_prenda= (Spinner) promptView.findViewById(R.id.spinner_tipo_de_prenda);
+        final Spinner spinner_tipo_prenda = (Spinner) promptView.findViewById(R.id.spinner_tipo_de_prenda);
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
                 R.array.tipo_de_prenda_array, android.R.layout.simple_spinner_item);
 // Specify the layout to use when the list of choices appears
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
-        spinner_tipo_prenda.setAdapter(adapter1);
+        spinner_tipo_prenda.setAdapter(
+                new NothingSelectedSpinnerAdapter(
+                        adapter1,
+                        R.layout.contact_spinner_row_nothing_selected,
+                        this));
 
-       final Spinner spinner_talla_ropa = (Spinner) promptView.findViewById(R.id.spinner_talla_ropa);
-        final Spinner spinner_colores = (Spinner) promptView.findViewById(R.id.spinner_color);
+        spinner_talla_ropa = (Spinner) promptView.findViewById(R.id.spinner_talla_ropa);
+        spinner_colores = (Spinner) promptView.findViewById(R.id.spinner_color);
+        otras_especificaciones = (EditText) promptView.findViewById(R.id.editText_comentario);
 
-        CheckBox checkBoxTalla = (CheckBox) promptView.findViewById(R.id.checkBox_talla);
-        CheckBox checkBoxColor= (CheckBox) promptView.findViewById(R.id.checkBox_color);
+        checkBoxTalla = (CheckBox) promptView.findViewById(R.id.checkBox_talla);
+        checkBoxColor = (CheckBox) promptView.findViewById(R.id.checkBox_color);
 
         alertDialogBuilder.setPositiveButton("Finalizar", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
+
+                String comentario = otras_especificaciones.getText().toString();
+
+                if(checkBoxTalla.isChecked()){
+                    String talla = spinner_talla_ropa.getSelectedItem().toString();
+                }
+
+                if (checkBoxTalla.isChecked()){
+                    String color = spinner_colores.getSelectedItem().toString();
+                }
+
+                //Método para guardar en la DB los valores de no venta
+
                 dialog.dismiss();
             }
         });
@@ -209,7 +242,6 @@ public class IniciarVentaActivity extends AppCompatActivity {
         alertDialogBuilder.setNeutralButton("Regresar", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 seleccionarResultado();
-
             }
         });
 
@@ -217,24 +249,36 @@ public class IniciarVentaActivity extends AppCompatActivity {
         final AlertDialog alert = alertDialogBuilder.create();
 
         spinner_tipo_prenda.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-             ArrayAdapter<CharSequence> adapterColores;
+            ArrayAdapter<CharSequence> adapterColores;
             ArrayAdapter<CharSequence> adapterTallas;
-
-
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
                 // your code here
-                if (position==1){ //primera opcion es una blusa, cargamos las opciones de blusa
-                    adapterTallas = ArrayAdapter.createFromResource(IniciarVentaActivity.this,
-                            R.array.tallas_blusa_array, android.R.layout.simple_spinner_item);
-                    adapterTallas.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                    spinner_talla_ropa.setAdapter(adapterTallas);
-
-                    adapterColores = ArrayAdapter.createFromResource(IniciarVentaActivity.this,
-                            R.array.colores_blusa_array, android.R.layout.simple_spinner_item);
-                    adapterColores.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                    spinner_colores.setAdapter(adapterColores);
-                }else{
+                if (position == 1) { //primera opcion es una blusa, cargamos las opciones de blusa
+                    LlenarSpinners(R.array.tallas_blusa_tshirt_array, R.array.colores_array);
+                }
+                else if (position == 2) {
+                    LlenarSpinners(R.array.tallas_blusa_tshirt_array, R.array.colores_array);
+                }
+                else if (position == 3) {
+                    LlenarSpinners(R.array.tallas_camisa_array, R.array.colores_array);
+                }
+                else if (position == 4) {
+                    LlenarSpinners(R.array.tallas_pantalones_array, R.array.colores_array);
+                }
+                else if (position == 5) {
+                    LlenarSpinners(R.array.tallas_zapatos_array, R.array.colores_array);
+                }
+                else if (position == 6) {
+                    LlenarSpinners(R.array.tallas_pantalones_array, R.array.colores_array);
+                }
+                else if (position == 7) {
+                    LlenarSpinners(R.array.tallas_blusa_tshirt_array, R.array.colores_array);
+                }
+                else if (position == 8) {
+                    LlenarSpinners(R.array.tallas_pantalones_array, R.array.colores_array);
+                }
+                else {
                     spinner_colores.setAdapter(null);
                     spinner_talla_ropa.setAdapter(null);
                 }
@@ -252,7 +296,7 @@ public class IniciarVentaActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     spinner_talla_ropa.setVisibility(View.VISIBLE);
-                }else {
+                } else {
                     spinner_talla_ropa.setVisibility(View.INVISIBLE);
                 }
             }
@@ -264,7 +308,7 @@ public class IniciarVentaActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     spinner_colores.setVisibility(View.VISIBLE);
-                }else {
+                } else {
                     spinner_colores.setVisibility(View.INVISIBLE);
                 }
             }
@@ -282,7 +326,7 @@ public class IniciarVentaActivity extends AppCompatActivity {
         alertDialogBuilder.setCancelable(false);
 
 
-        EditText totalEditText= (EditText) promptView.findViewById(R.id.editText_total);
+        EditText totalEditText = (EditText) promptView.findViewById(R.id.editText_total);
 
         alertDialogBuilder.setPositiveButton("Finalizar", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
@@ -301,6 +345,29 @@ public class IniciarVentaActivity extends AppCompatActivity {
         alert.show();
     }
 
+    public void LlenarSpinners(int arrayTallas,  int arrayColores){
+
+        ArrayAdapter<CharSequence> adapterColores;
+        ArrayAdapter<CharSequence> adapterTallas;
+
+        adapterTallas = ArrayAdapter.createFromResource(IniciarVentaActivity.this,
+                arrayTallas, android.R.layout.simple_spinner_item);
+        adapterTallas.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_talla_ropa.setAdapter(
+                new NothingSelectedSpinnerAdapter(
+                        adapterTallas,
+                        R.layout.contact_spinner_row_nothing_selected,
+                        this));
+
+        adapterColores = ArrayAdapter.createFromResource(IniciarVentaActivity.this,
+                arrayColores, android.R.layout.simple_spinner_item);
+        adapterColores.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_colores.setAdapter(
+                new NothingSelectedSpinnerAdapter(
+                        adapterColores,
+                        R.layout.contact_spinner_row_nothing_selected,
+                        this));
+    }
 
     public void recordAudio(View v) {
         CallWebService llamada = new CallWebService();
@@ -323,22 +390,22 @@ public class IniciarVentaActivity extends AppCompatActivity {
 
     //*********************CODIGO AGREGADO POR KEVIN ******************************************//
 
-    public void uploadFile(int requestCode, int resultCode, final Intent data){
+    public void uploadFile(int requestCode, int resultCode, final Intent data) {
         Thread t = new Thread(new Runnable() {
             @Override
             public void run() {
 
-                File f  = new File(AUDIO_FILE_PATH);
-                String content_type  = getMimeType(f.getPath());
+                File f = new File(AUDIO_FILE_PATH);
+                String content_type = getMimeType(f.getPath());
 
                 String file_path = f.getAbsolutePath();
                 OkHttpClient client = new OkHttpClient();
-                RequestBody file_body = RequestBody.create(MediaType.parse(content_type),f);
+                RequestBody file_body = RequestBody.create(MediaType.parse(content_type), f);
 
                 RequestBody request_body = new MultipartBody.Builder()
                         .setType(MultipartBody.FORM)
-                        .addFormDataPart("type",content_type)
-                        .addFormDataPart("uploaded_file",file_path.substring(file_path.lastIndexOf("/")+1), file_body)
+                        .addFormDataPart("type", content_type)
+                        .addFormDataPart("uploaded_file", file_path.substring(file_path.lastIndexOf("/") + 1), file_body)
                         .build();
 
                 Request request = new Request.Builder()
@@ -348,8 +415,8 @@ public class IniciarVentaActivity extends AppCompatActivity {
 
                 try {
                     Response response = client.newCall(request).execute();
-                    if(!response.isSuccessful()){
-                        throw new IOException("Error : "+response);
+                    if (!response.isSuccessful()) {
+                        throw new IOException("Error : " + response);
                     }
 
                 } catch (IOException e) {
@@ -362,19 +429,21 @@ public class IniciarVentaActivity extends AppCompatActivity {
 
         t.start();
     }
+
     private String getMimeType(String path) {
         String extension = MimeTypeMap.getFileExtensionFromUrl(path);
         return MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
     }
 
     class CallWebService extends AsyncTask<String, Void, String> {
+        String resultado;
+
         @Override
         protected void onPostExecute(String s) {
             // text.setText("Square = " + s);
         }
 
-        String resultado;
-        private String iniciarVenta(){
+        private String iniciarVenta() {
             String result = "";
             SoapObject soapObject = new SoapObject(variables.NAMESPACE, "crearVenta");
             SoapObject soapObject1 = new SoapObject("", "");
@@ -387,21 +456,21 @@ public class IniciarVentaActivity extends AppCompatActivity {
             item.setValue(soapObject1);
 
             soapObject.addProperty(item);
-            SoapSerializationEnvelope envelope =  new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
             envelope.setOutputSoapObject(soapObject);
             HttpTransportSE httpTransportSE = new HttpTransportSE(variables.URL);
             try {
                 httpTransportSE.call(variables.SOAP_ACTION, envelope);
-                SoapObject response=(SoapObject)envelope.bodyIn;
-                SoapObject r2 = (SoapObject)response.getProperty(0);
-                result  = r2.getProperty(0).toString();
+                SoapObject response = (SoapObject) envelope.bodyIn;
+                SoapObject r2 = (SoapObject) response.getProperty(0);
+                result = r2.getProperty(0).toString();
             } catch (Exception e) {
                 e.printStackTrace();
             }
             return result;
         }
 
-        private String finVenta(){
+        private String finVenta() {
             String result = "";
             SoapObject soapObject = new SoapObject(variables.NAMESPACE, "finalizarVenta");
             SoapObject soapObject1 = new SoapObject("", "");
@@ -413,21 +482,21 @@ public class IniciarVentaActivity extends AppCompatActivity {
             item.setValue(soapObject1);
 
             soapObject.addProperty(item);
-            SoapSerializationEnvelope envelope =  new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
             envelope.setOutputSoapObject(soapObject);
             HttpTransportSE httpTransportSE = new HttpTransportSE(variables.URL);
             try {
                 httpTransportSE.call(variables.SOAP_ACTION, envelope);
-                SoapObject response=(SoapObject)envelope.bodyIn;
-                SoapObject r2 = (SoapObject)response.getProperty(0);
-                result  = r2.getProperty(0).toString();
+                SoapObject response = (SoapObject) envelope.bodyIn;
+                SoapObject r2 = (SoapObject) response.getProperty(0);
+                result = r2.getProperty(0).toString();
             } catch (Exception e) {
                 e.printStackTrace();
             }
             return result;
         }
 
-        private String cerrarSesion(){
+        private String cerrarSesion() {
             String result = "";
             SoapObject soapObject = new SoapObject(variables.NAMESPACE, "cerrarSesion");
             SoapObject soapObject1 = new SoapObject("", "");
@@ -441,24 +510,25 @@ public class IniciarVentaActivity extends AppCompatActivity {
             item.setValue(soapObject1);
 
             soapObject.addProperty(item);
-            SoapSerializationEnvelope envelope =  new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
             envelope.setOutputSoapObject(soapObject);
             HttpTransportSE httpTransportSE = new HttpTransportSE(variables.URL);
             try {
                 httpTransportSE.call(variables.SOAP_ACTION, envelope);
-                SoapObject response=(SoapObject)envelope.bodyIn;
-                SoapObject r2 = (SoapObject)response.getProperty(0);
-                result  = r2.getProperty(0).toString();
+                SoapObject response = (SoapObject) envelope.bodyIn;
+                SoapObject r2 = (SoapObject) response.getProperty(0);
+                result = r2.getProperty(0).toString();
             } catch (Exception e) {
                 e.printStackTrace();
             }
             return result;
         }
+
         @Override
         protected String doInBackground(String... params) {
             String result = "";
 
-            switch (params[0]){
+            switch (params[0]) {
                 case "1":
                     result = iniciarVenta();
                     try {
@@ -479,7 +549,7 @@ public class IniciarVentaActivity extends AppCompatActivity {
                     break;
                 case "3":
                     result = cerrarSesion();
-                    if(result.equals("1")){
+                    if (result.equals("1")) {
                         finish();
                     }
                     break;
